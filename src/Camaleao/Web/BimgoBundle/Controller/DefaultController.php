a7a23c68f69c1f7af5f4f88c53576a6cfcbd3555
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2016-03-14 17:44:19 -0300

    inicio do menu e da pagina incial

--- src/Camaleao/Web/BimgoBundle/Controller/DefaultController.php
+++ src/Camaleao/Web/BimgoBundle/Controller/DefaultController.php
@@ -3,9 +3,22 @@
 namespace Camaleao\Web\BimgoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Default controller.
+ *
+ * @Route("/")
+ */
 class DefaultController extends Controller
 {
+    /**
+     *
+     *
+     * @Route("/", name="default_index")
+     * @Method("GET")
+     */
     public function indexAction()
     {
         return $this->render('CamaleaoWebBimgoBundle:Default:index.html.twig');
