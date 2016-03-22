16b0f8183135fba6c7a3785f4675ffedbeadb71d
Pedro Augusto <pedro.e@ufv.br>
2016-03-22 17:16:38 -0300

    ajudando o new estado para receber do android

--- src/Camaleao/Web/BimgoBundle/Controller/AndroidController.php
+++ src/Camaleao/Web/BimgoBundle/Controller/AndroidController.php
@@ -17,51 +17,6 @@ use Camaleao\Web\BimgoBundle\Entity\Empresa;
 class AndroidController extends Controller
 {
     /**
-     * load empresas for android
-     *
-     * @Route("/getempresas", name="android_getempresas")
-     * @Method("POST")
-     */
-    public function getEmpresasAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $empresas = $em->getRepository('CamaleaoWebBimgoBundle:Empresa')->findAll();
-
-        $serializer = $this->container->get('jms_serializer');
-
-        $reports = $serializer->serialize($empresas, 'json');
-
-        return new Response($reports);
-    }
-
-    /**
-     * load empresas for android
-     *
-     * @Route("/getempresas", name="android_getempresas")
-     * @Method("POST")
-     */
-    public function setEmpresaAction(Request $request)
-    {
-        // recupera parametros
-        $params = $request->getParams();
-
-        // instancia o a entidade empresa
-        $empresa = new Empresa();
-
-        // popula a entidade
-        $empresa->bind($params['empresa']);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($empresa);
-        $em->flush();
-
-        $return = json_encode(array('result' => true));
-
-        return new Response($return);
-    }
-
-    /**
      *  select em estados
      *
      * @Route("/getestados", name="android_getestados")
@@ -84,19 +39,13 @@ class AndroidController extends Controller
 
     /**
      * insert em estado
-     *  {"nome":"Clevao","uf":"CC"}
+     * {"nome":"Clevao","uf":"CC"}
      *
      * @Route("/newestado", name="android_newestado")
      * @Method({"GET", "POST"})
      */
     public function newEstadoAction(Request $request)
     {
-        $response =  new Response($request, Response::HTTP_OK, array('content-type' => 'application/json') );
-
-        $response->send();
-
-        dump($response); exit;
-
         $estado = new Estado();
         $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\EstadoType', $estado);
         $form->handleRequest($request);
