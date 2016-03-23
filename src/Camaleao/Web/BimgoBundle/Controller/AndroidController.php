21b0faf85633e2ff02503635ab56272165b6fc5a
cleverson <cleverson.trevenzoli@ufv.br>
2016-03-23 16:01:48 -0300

    teste de insercao de estado pelo android

--- src/Camaleao/Web/BimgoBundle/Controller/AndroidController.php
+++ src/Camaleao/Web/BimgoBundle/Controller/AndroidController.php
@@ -32,34 +32,32 @@ class AndroidController extends Controller
 
         $serializer = $this->container->get('jms_serializer');
 
-        $reports = $serializer->serialize($array, 'json');
+        $result = $serializer->serialize($array, 'json');
 
-        return new Response($reports, Response::HTTP_OK, array('content-type' => 'application/json') );
+        return new Response($result, Response::HTTP_OK, array('content-type' => 'application/json'));
     }
 
     /**
      * insert em estado
-     * {"nome":"Clevao","uf":"CC"}
      *
      * @Route("/newestado", name="android_newestado")
      * @Method({"GET", "POST"})
      */
     public function newEstadoAction(Request $request)
     {
+        $result = false;
         $estado = new Estado();
         $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\EstadoType', $estado);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($estado);
             $em->flush();
 
-            return $this->redirectToRoute('estado_show', array('id' => $estado->getId()));
+            $result = true;
         }
 
-        $return = json_encode(array('result' => true));
-
-        return new Response($return);
+        return new Response(json_encode(array('result' => $result)), Response::HTTP_OK, array('content-type' => 'application/json'));
     }
 }
