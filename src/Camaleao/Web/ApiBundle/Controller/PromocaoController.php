ed06890aa05e19e1e9b1d4c8049373bdcc41e378
Pedro Augusto <pedro.e@ufv.br>
2016-05-10 17:57:26 -0300

    unicao das actions de patch e put para promocao

--- src/Camaleao/Web/ApiBundle/Controller/PromocaoController.php
+++ src/Camaleao/Web/ApiBundle/Controller/PromocaoController.php
@@ -128,7 +128,7 @@ class PromocaoController extends Controller
      * @return Response
      *
      * @Route("/{id}", name="api_v1_promocoes_edit")
-     * @Method("PUT")
+     * @Method({"PATCH", "PUT"})
      */
     public function editAction(Request $request, Promocao $promocao)
     {
@@ -143,52 +143,7 @@ class PromocaoController extends Controller
             }
         }
 
-        $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\PromocaoType', $promocao, array('method' => 'PUT'));
-        $form->handleRequest($request);
-
-        if (!$form->isValid()) {
-            $response->setStatusCode(Response::HTTP_PRECONDITION_FAILED);
-            $responseContent = $serializer->serialize($form->getErrors(true), 'json');
-            $response->setContent($responseContent);
-            return $response;
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $promocao = $em->merge($promocao);
-        $em->flush();
-
-        $responseContent = $serializer->serialize($promocao, 'json');
-        $response->setContent($responseContent);
-        $response->setStatusCode(Response::HTTP_OK);
-        $response->headers->set('Location', $this->generateUrl('api_v1_promocoes_show', array('id' => $promocao->getId()), true));
-
-        return $response;
-    }
-
-    /**
-     * Update an field from existing Promocao entity.
-     *
-     * @param Request $request
-     * @param Promocao $promocao
-     * @return Response
-     *
-     * @Route("/{id}", name="api_v1_promocoes_update")
-     * @Method("PATCH")
-     */
-    public function updateAction(Request $request, Promocao $promocao)
-    {
-        $response = new Response();
-        $serializer = $this->container->get('jms_serializer');
-
-        if($request->getContentType() == 'json') {
-            $response->headers->set('Content-Type', 'application/json');
-            if($request->getContent()) {
-                $requestContent = json_decode($request->getContent(), true);
-                $request->request->replace($requestContent);
-            }
-        }
-
-        $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\PromocaoType', $promocao, array('method' => 'PATCH'));
+        $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\PromocaoType', $promocao, array('method' => $request->getMethod()));
         $form->handleRequest($request);
 
         if (!$form->isValid()) {
