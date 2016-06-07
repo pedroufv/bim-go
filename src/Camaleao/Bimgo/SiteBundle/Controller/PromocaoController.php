a726ab6b3b5f0cc3616a9b5a5b6dbf333fd72640
Pedro Augusto <pams.pedro@gmail.com>
2016-06-06 23:06:12 -0300

    criacao do promocao controler e exibicao da sessao de recentes - template - de promocao na pagina inicial

--- src/Camaleao/Bimgo/SiteBundle/Controller/PromocaoController.php
+++ src/Camaleao/Bimgo/SiteBundle/Controller/PromocaoController.php
@@ -6,8 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Camaleao\Bimgo\CoreBundle\Entity\Promocao;
-//use Camaleao\Web\BimgoBundle\Form\PromocaoType;
 
 /**
  * Promocao controller.
@@ -19,7 +19,7 @@ class PromocaoController extends Controller
     /**
      * Lists all Promocao entities.
      *
-     * @Route("/", name="promocao_index")
+     * @Route(name="site_promocao_index")
      * @Method("GET")
      */
     public function indexAction(Request $request)
@@ -38,35 +38,9 @@ class PromocaoController extends Controller
     }
 
     /**
-     * Creates a new Promocao entity.
-     *
-     * @Route("/new", name="promocao_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $promocao = new Promocao();
-        $form = $this->createForm('Camaleao\Web\BimgoBundle\Form\PromocaoType', $promocao);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($promocao);
-            $em->flush();
-
-            return $this->redirectToRoute('promocao_show', array('id' => $promocao->getId()));
-        }
-
-        return $this->render('CamaleaoWebBimgoBundle:promocao:new.html.twig', array(
-            'promocao' => $promocao,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
      * Finds and displays a Promocao entity.
      *
-     * @Route("/{id}", name="promocao_show")
+     * @Route("/{id}", name="site_promocao_show")
      * @Method("GET")
      */
     public function showAction(Promocao $promocao)
@@ -80,65 +54,20 @@ class PromocaoController extends Controller
     }
 
     /**
-     * Displays a form to edit an existing Promocao entity.
+     * Lists recent Promocao entities.
      *
-     * @Route("/{id}/edit", name="promocao_edit")
-     * @Method({"GET", "POST"})
+     * @Route(name="site_promocao_recentsection")
+     * @Method("GET")
+     * @Template()
      */
-    public function editAction(Request $request, Promocao $promocao)
+    public function recentSectionAction($max = 4)
     {
-        $deleteForm = $this->createDeleteForm($promocao);
-        $editForm = $this->createForm('Camaleao\Web\BimgoBundle\Form\PromocaoType', $promocao);
-        $editForm->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($promocao);
-            $em->flush();
+        $promocoes = $em->getRepository('CamaleaoBimgoCoreBundle:Promocao')->findBy(array('publicada' => true), array('id' => 'DESC'), $max);
 
-            return $this->redirectToRoute('promocao_edit', array('id' => $promocao->getId()));
-        }
-
-        return $this->render('CamaleaoWebBimgoBundle:promocao:edit.html.twig', array(
-            'promocao' => $promocao,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+        return $this->render('CamaleaoBimgoSiteBundle:promocao:recentSection.html.twig', array(
+            'promocoes' => $promocoes,
         ));
-    }
-
-    /**
-     * Deletes a Promocao entity.
-     *
-     * @Route("/{id}", name="promocao_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Promocao $promocao)
-    {
-        $form = $this->createDeleteForm($promocao);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($promocao);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('promocao_index');
-    }
-
-    /**
-     * Creates a form to delete a Promocao entity.
-     *
-     * @param Promocao $promocao The Promocao entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Promocao $promocao)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('promocao_delete', array('id' => $promocao->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
