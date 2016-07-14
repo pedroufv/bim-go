fb46f7b751eebcce0f0af9b07a7573e859a6957b
Pedro Augusto <pams.pedro@gmail.com>
2016-07-13 22:12:15 -0300

    criacao da action de reporte para disparar emails com avisos do clientes sobre correcoes - um email eh enviado ao cliente e outro com o reporte para a equipe bim-go

--- src/Camaleao/Bimgo/ApiBundle/Controller/InstituicaoController.php
+++ src/Camaleao/Bimgo/ApiBundle/Controller/InstituicaoController.php
@@ -3,6 +3,7 @@
 namespace Camaleao\Bimgo\ApiBundle\Controller;
 
 use Camaleao\Bimgo\CoreBundle\Entity\Instituicao;
+use Camaleao\Bimgo\CoreBundle\Entity\Reporte;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -393,6 +394,77 @@ class InstituicaoController extends ApiController
 
         $serializer = $this->container->get('jms_serializer');
         $result = $serializer->serialize($content, 'json');
+
+        $response = new Response($result);
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @Route("/reportar", name="api_v1_instituicoes_reportar")
+     * @Method("POST")
+     */
+    public function reportAction(Request $request)
+    {
+        $response = new Response();
+        $serializer = $this->container->get('jms_serializer');
+
+        if(!$request->getContent()){
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+            return $response;
+        }
+
+        if($request->getContentType() == 'json') {
+            $requestContent = json_decode($request->getContent(), true);
+            if(!$requestContent) {
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+                return $response;
+            }
+            $options = array('csrf_protection' => false);
+            $response->headers->set('Content-Type', 'application/json');
+            $request->request->replace($requestContent);
+        }
+
+        $reporte = new Reporte();
+        $form = $this->createForm('Camaleao\Bimgo\CoreBundle\Form\ReporteType', $reporte, $options);
+        $form->handleRequest($request);
+
+        if (!$form->isValid()) {
+            $response->setStatusCode(Response::HTTP_PRECONDITION_FAILED);
+            $responseContent = $serializer->serialize($form->getErrors(true), 'json');
+            $response->setContent($responseContent);
+            return $response;
+        }
+
+        $agradecimento = \Swift_Message::newInstance()
+            ->setSubject('Bim-go! - Obrigado pela sua contribuição')
+            ->setFrom('cpe.feroz@gmail.com')
+            ->setTo($reporte->getEmailRemetente())
+            ->setBody(
+                $this->renderView('CamaleaoBimgoApiBundle:email:congratulation.html.twig', array('reporte' => $reporte)),
+                "text/html"
+            )
+        ;
+        $this->get('mailer')->send($agradecimento);
+
+        $envioReporte = \Swift_Message::newInstance()
+            ->setSubject('[Bim-go! - Reporte] '.$reporte->getCategoria())
+            ->setFrom($reporte->getEmailRemetente())
+            ->setReplyTo($reporte->getEmailRemetente())
+            ->setTo('cpe.feroz@gmail.com')
+            ->setBody(
+                $this->renderView('CamaleaoBimgoApiBundle:email:report.html.twig', array('reporte' => $reporte)),
+                "text/html"
+            )
+        ;
+        $this->get('mailer')->send($envioReporte);
+
+        $result = $serializer->serialize(array('success' => true), 'json');
 
         $response = new Response($result);
         $response->setStatusCode(Response::HTTP_OK);
---
+++ src/Camaleao/Bimgo/CoreBundle/Entity/Reporte.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Camaleao\Bimgo\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reporte
+ *
+ */
+class Reporte
+{
+    /**
+     * @var string
+     */
+    private $nomeRemetente;
+
+    /**
+     * @var string
+     */
+    private $emailRemetente;
+
+    /**
+     * @var string
+     */
+    private $categoria;
+
+    /**
+     * @var string
+     */
+    private $mensagem;
+
+    /**
+     * @return string
+     */
+    public function getNomeRemetente()
+    {
+        return $this->nomeRemetente;
+    }
+
+    /**
+     * @param string $nomeRemetente
+     */
+    public function setNomeRemetente($nomeRemetente)
+    {
+        $this->nomeRemetente = $nomeRemetente;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmailRemetente()
+    {
+        return $this->emailRemetente;
+    }
+
+    /**
+     * @param string $emailRemetente
+     */
+    public function setEmailRemetente($emailRemetente)
+    {
+        $this->emailRemetente = $emailRemetente;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * @param string $categoria
+     */
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMensagem()
+    {
+        return $this->mensagem;
+    }
+
+    /**
+     * @param string $mensagem
+     */
+    public function setMensagem($mensagem)
+    {
+        $this->mensagem = $mensagem;
+    }
+}
